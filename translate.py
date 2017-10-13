2618fbacb1aa12c82b5901c3ef28ba9daaec5e39
Joel <joel@helm.cat>
2017-10-13 12:04:45 +0200

    Improves translate script

--- translate.py
+++ translate.py
@@ -5,25 +5,28 @@ import os
 
 OPEN_TAG = "<lang>"
 CLOSE_TAG = "</lang>"
-LOCALE_FILES_PATH = "../assets/locales/"
+LOCALE_FILES_PATH = "locales/"
 INPUT_FILE_PATH = "index.html"
 OUTPUT_FILE_NAME = "index.html"
 
-def translateTo(language):
-    translatedHtml = baseHtml
-    translationFilePath = TRANSLATION_FILES_PATH + language + '.json'
-    translationFile = open(translationFilePath, 'r')
-    translationJson = json.load(translationFile)
+def translate(textToTranslate, localeFilePath, outputFolder):
+    translatedText = textToTranslate
+    localeFile = open(localeFilePath, 'r')
+    try:
+        localeJson = json.load(localeFile)
+    except:
+        print "Cannot parse file '" + localeFilePath + "'as JSON"
+        return
 
     for key in keys:
         textToSearch = OPEN_TAG + key + CLOSE_TAG
         try:
-            textToReplace = translationJson["token"][key]
-            translatedHtml = translatedHtml.replace(textToSearch, textToReplace.encode('utf-8'))
+            textToReplace = localeJson[key]
+            translatedText = translatedText.replace(textToSearch, textToReplace.encode('utf-8'))
         except:
             print "ERROR: The key '" + key + "'does not exist"
 
-    translatedFilePath = './' + language + '/' + OUTPUT_FILE_NAME
+    translatedFilePath = './' + outputFolder + '/' + OUTPUT_FILE_NAME
     if not os.path.exists(os.path.dirname(translatedFilePath)):
         try:
             os.makedirs(os.path.dirname(translatedFilePath))
@@ -32,17 +35,19 @@ def translateTo(language):
             raise
 
     translatedFile = open(translatedFilePath, 'w')
-    translatedFile.write(translatedHtml)
+    translatedFile.write(translatedText)
     translatedFile.close()
 
 
-baseFile = open(BASE_FILE_PATH, 'r')
+baseFile = open(INPUT_FILE_PATH, 'r')
 baseHtml = baseFile.read()
 baseFile.close()
 
 findKeyRegex = OPEN_TAG + "(.+?)" + CLOSE_TAG
 keys = re.findall(findKeyRegex, baseHtml)
 
-translateTo('es')
-translateTo('ca')
-translateTo('en')
+for file in os.listdir(LOCALE_FILES_PATH):
+    if file.endswith(".json"):
+        filename = os.path.splitext(file)[0]
+        path = os.path.join(LOCALE_FILES_PATH, file)
+        translate(baseHtml, path, filename)
